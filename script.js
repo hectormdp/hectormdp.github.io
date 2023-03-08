0bae7b18f2c2d07cdc2bf8cc5635f37a98eb4da2
Héctor7600 <hchaves@gmail.com>
2023-03-07 21:44:08 -0300

    origin/main

--- script.js
+++ script.js
@@ -23,14 +23,14 @@ function efectoHabilidades(){
         let habilidades = document.getElementsByClassName("progreso");
         habilidades[0].classList.add("javascript");
         habilidades[1].classList.add("htmlcss");
-        habilidades[2].classList.add("photoshop");
-        habilidades[3].classList.add("wordpress");
-        habilidades[4].classList.add("drupal");
+        habilidades[2].classList.add("java");
+        habilidades[3].classList.add("phyton");
+        habilidades[4].classList.add("bootstrap");
         habilidades[5].classList.add("comunicacion");
         habilidades[6].classList.add("trabajo");
         habilidades[7].classList.add("creatividad");
-        habilidades[8].classList.add("dedicacion");
-        habilidades[9].classList.add("proyect");
+        habilidades[8].classList.add("empatia");
+        habilidades[9].classList.add("organización");
     }
 }
 
